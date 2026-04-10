2593000b3531e8115bc25c283f06cc1256bd9d38
Ramon21Empresa <ramon21.empresa@gmail.com>
2026-04-10 15:15:13 -0300

    Delete frontend directory

--- frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Permite importar CSS do Leaflet
-  transpilePackages: ['leaflet', 'react-leaflet'],
-}
-
-module.exports = nextConfig
